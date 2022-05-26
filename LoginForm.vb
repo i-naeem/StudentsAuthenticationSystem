19ad0c0a731e6504d04cfdf5877ec0013078dd39
i-naeem <inaeem@protonmail.com>
2022-05-26 13:29:58 +0500

    :sparkles: add functionality to login form

--- LoginForm.vb
+++ LoginForm.vb
@@ -1,5 +1,20 @@
 ﻿Public Class LoginForm
+    Dim rollNo As Integer
+    Dim password As String
+
     Private Sub LoginForm_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         Application.Exit()
+    End Sub
+
+    Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
+        rollNo = RollNoTextBox.Text
+        password = PasswordTextBox.Text
+
+        MessageBox.Show("ROLL NO: " + Convert.ToString(rollNo) + " password " + password)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Home.Show()
     End Sub
 End Class
