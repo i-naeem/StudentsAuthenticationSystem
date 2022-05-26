9940b62be8a75cc5d2a6aec5533edead3b0341da
i-naeem <inaeem@protonmail.com>
2022-05-26 20:42:53 +0500

    :sparkles: add connection string

--- Home.vb
+++ Home.vb
@@ -1,4 +1,9 @@
-﻿Public Class Home
+﻿Imports System.Data.SqlClient
+
+Public Class Home
+    Private ReadOnly connectionString As String = "Server=Localhost\SQLExpress;Database=Students;Integrated Security=true"
+    Public connection As New SqlConnection(connectionString)
+
     Private Sub LoginAnchorButton_Click(sender As Object, e As EventArgs) Handles LoginAnchorButton.Click
         Me.Hide()
         LoginForm.Show()
