a3f6c502940ae076e8694fc662bd83e7ba6a34e5
i-naeem <inaeem@protonmail.com>
2022-05-26 13:33:17 +0500

    :sparkles: make signup form functional

--- SignupForm.vb
+++ SignupForm.vb
@@ -1,5 +1,22 @@
 ﻿Public Class SignupForm
+    Dim rollNo As Integer
+    Dim fname As String
+    Dim password As String
+
     Private Sub SignupForm_FormClosed(sender As Object, e As FormClosedEventArgs) Handles MyBase.FormClosed
         Application.Exit()
+    End Sub
+
+    Private Sub SignupButton_Click(sender As Object, e As EventArgs) Handles SignupButton.Click
+        rollNo = RollNoTextBox.Text
+        fname = FullNameTextBox.Text
+        password = PasswordTextBox.Text
+
+        MessageBox.Show("Roll No: " + Convert.ToString(rollNo) + " Full Name: " + fname + " Password: " + password)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        Home.Show()
     End Sub
 End Class
